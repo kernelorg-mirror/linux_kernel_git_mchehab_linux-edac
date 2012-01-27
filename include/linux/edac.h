64908a6baf70a775bdf70962e425ef11c89db750
Mauro Carvalho Chehab <mchehab@redhat.com>
2012-01-27 10:26:13 -0300

    drivers/edac: rename channel_info to csrow_channel_info
    Newer memory architectures use the term "channel" with a different
    meaning.
    
    On a traditional architecture, the memory controller directly
    access the memory ranks, via chip select rows.
    
    The memory controller and the memory configuration can be single-channel
    or double-channel. Single-channel means 64 bits access, while dual-channel
    means 128 parallel access, provided by two memory sticks that are accessed
    at the same time. The addresses between channel A and channel B are
    interleaved. The memories on each channel should be identical for it
    to work.
    
    When two channels are provided, the DIMM memories are generally called
    DIMM 1A/DIMM 1B (where 1A means DIMM 1 at channel A, and so on).
    
    On some modern memory architectures like FB-DIMM, there's a microcontroller
    chip, called Advanced Memory Buffer (AMB) that serves as the interface
    between the memory controller and the memory chips. So, the memory
    controller sees independent memory channels, and doesn't actually select
    the memory by a "chip select". Instead, it passes the DIMM slot it wants
    to access to the AMB.
    
    It is up to the AMB to talk with the csrows of the DRAM chips.
    
    The bus that exchanges information between the memory controller and
    the CPU is also called "channel", but it is not associated with
    the channel interleaving, as each channel is independent. The entire
    csrow concept is not even visible to the memory controller, as using
    csrows is a task for the AMB.
    
    Newer memory controllers like the one found on Intel Sandy Bridge
    processors, even when working with normal DDR3 DIMM's, don't use the
    channel A/channel B interleaving schema to provide 128 bits. Instead,
    they have more channels (3 or 4 channels), and several interleaving
    schemas are supported, and a csrow concept is not directly visible by
    the memory controller.
    
    The drivers that support such such newer memory architecture models
    currently need to fake information and to abuse on EDAC structures,
    as the subsystem was conceived with the idea that the csrow would always
    be visible by the CPU.
    
    To make things a little worse, those drivers don't currently fake
    csrows/channels on a consistent way, as the concepts there don't
    apply to the memory controllers they're talking with.
    
    In order to fix it, let's rename "channel" to "cschannel", in order to
    be clearer that this channel info will only be used when the csrows
    concept applies.
    
    Latter patchsets will provide a better way to represent the memory
    hierarchy on a way that will work with other memory architectures.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/linux/edac.h
+++ include/linux/edac.h
@@ -191,7 +191,7 @@ enum scrub_type {
  * Socket:		A physical connector on the motherboard that accepts
  *			a single memory stick.
  *
- * Channel:		Set of memory devices on a memory stick that must be
+ * Csrow-channel:	Set of memory devices on a memory stick that must be
  *			grouped in parallel with one or more additional
  *			channels from other memory sticks.  This parallel
  *			grouping of the output from multiple channels are
@@ -249,7 +249,7 @@ enum scrub_type {
  * PS - I enjoyed writing all that about as much as you enjoyed reading it.
  */
 
-struct channel_info {
+struct csrow_channel_info {
 	int chan_idx;		/* channel index */
 	u32 ce_count;		/* Correctable Errors for this CHANNEL */
 	char label[EDAC_MC_LABEL_LEN + 1];	/* DIMM label on motherboard */
@@ -276,7 +276,7 @@ struct csrow_info {
 
 	/* channel information for this csrow */
 	u32 nr_channels;
-	struct channel_info *channels;
+	struct csrow_channel_info *channels;
 };
 
 struct mcidev_sysfs_group {
