189cb9d18f67a29ba8eb71fd390f4f1125c57959
Mauro Carvalho Chehab <mchehab@redhat.com>
2012-01-27 15:48:37 -0300

    edac: Add per dimm's sysfs nodes
    Instead of just exporting a per-csrow dimm directories, add
    a pure per-dimm attributes. This will help to better map
    the DIMM properties, when csrow info is not available.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- include/linux/edac.h
+++ include/linux/edac.h
@@ -268,6 +268,8 @@ struct dimm_info {
 			unsigned csrow_channel;
 		};
 	} location;
+	struct kobject kobj;		/* sysfs kobject for this csrow */
+	struct mem_ctl_info *mci;	/* the parent */
 };
 
 struct csrow_channel_info {
