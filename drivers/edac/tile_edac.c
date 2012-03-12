00c615ddd4d008e9b3a66ef1ad55b7ff2ea44097
Mauro Carvalho Chehab <mchehab@redhat.com>
2012-03-12 10:45:39 -0300

    tile_edac: Fix the memory controller ID
    Fix a regression introduced by changeset 7b03829a8ba4:
    
    -       mci = edac_mc_alloc(sizeof(struct tile_edac_priv),
    -               TILE_EDAC_NR_CSROWS, TILE_EDAC_NR_CHANS, pdev->id);
    +       mci = edac_mc_alloc(0, ARRAY_SIZE(layers), layers, false,
    +                           sizeof(struct tile_edac_priv));
    
    The pdev->id is missing.
    
    Compile-tested only. Btw, in order to compile it, I had to:
    
    1) comment a header at arch/tile/include/asm/linkage.h:
    
    	-#include <feedback.h>
    	+//#include <feedback.h>
    
    2) Use the ./arch/tile/configs/tilegx_defconfig . the tilepro_defconfig
       is broken;
    
    3) manually enable the EDAC module on it.
    
    Somebody has to fix it ;)
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/edac/tile_edac.c
+++ drivers/edac/tile_edac.c
@@ -141,7 +141,7 @@ static int __devinit tile_edac_mc_probe(struct platform_device *pdev)
 	layers[1].type = EDAC_MC_LAYER_CHANNEL;
 	layers[1].size = TILE_EDAC_NR_CHANS;
 	layers[1].is_csrow = false;
-	mci = edac_mc_alloc(0, ARRAY_SIZE(layers), layers, false,
+	mci = edac_mc_alloc(pdev->id, ARRAY_SIZE(layers), layers, false,
 			    sizeof(struct tile_edac_priv));
 	if (mci == NULL)
 		return -ENOMEM;
