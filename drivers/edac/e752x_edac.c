4a5f85e7d2b93fdccf017cd3002f82254756d45c
Mauro Carvalho Chehab <mchehab@redhat.com>
2012-04-16 15:24:09 -0300

    edac: Remove the legacy EDAC ABI
    Now that all drivers got converted to use the new ABI, we can
    drop the old one.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/edac/e752x_edac.c
+++ drivers/edac/e752x_edac.c
@@ -1278,7 +1278,7 @@ static int e752x_probe1(struct pci_dev *pdev, int dev_idx)
 	layers[1].type = EDAC_MC_LAYER_CHANNEL;
 	layers[1].size = drc_chan + 1;
 	layers[1].is_csrow = false;
-	mci = new_edac_mc_alloc(0, ARRAY_SIZE(layers), layers,
+	mci = edac_mc_alloc(0, ARRAY_SIZE(layers), layers,
 			    false, sizeof(*pvt));
 	if (mci == NULL)
 		return -ENOMEM;
