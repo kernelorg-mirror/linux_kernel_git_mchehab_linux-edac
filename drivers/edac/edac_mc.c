ca154bf4bb0ee52ef1d43baa576c640620e12492
Mauro Carvalho Chehab <mchehab@redhat.com>
2013-02-19 21:35:41 -0300

    ghes_edac: Fix RAS tracing
    With the current version of CPER, there's no way to associate an
    error with the memory error. So, the error location in EDAC
    layers is unused.
    
    As CPER has its own idea about memory architectural layers, just
    output whatever is there inside the driver's detail at the RAS
    tracepoint.
    
    The EDAC location keeps untouched, in the case that, in some future,
    we could actually map the error into the dimm labels.
    
    Now, the error message:
    
    [   72.396625] {1}[Hardware Error]: Hardware error from APEI Generic Hardware Error Source: 0
    [   72.396627] {1}[Hardware Error]: APEI generic hardware error status
    [   72.396628] {1}[Hardware Error]: severity: 2, corrected
    [   72.396630] {1}[Hardware Error]: section: 0, severity: 2, corrected
    [   72.396632] {1}[Hardware Error]: flags: 0x01
    [   72.396634] {1}[Hardware Error]: primary
    [   72.396635] {1}[Hardware Error]: section_type: memory error
    [   72.396637] {1}[Hardware Error]: error_status: 0x0000000000000400
    [   72.396638] {1}[Hardware Error]: node: 3
    [   72.396639] {1}[Hardware Error]: card: 0
    [   72.396640] {1}[Hardware Error]: module: 0
    [   72.396641] {1}[Hardware Error]: device: 0
    [   72.396643] {1}[Hardware Error]: error_type: 18, unknown
    [   72.396666] EDAC MC0: 1 CE reserved error (18) on unknown label (node:3 card:0 module:0 page:0x0 offset:0x0 grain:0 syndrome:0x0 - status(0x0000000000000400): Storage error in DRAM memory)
    
    Is properly represented on the trace event:
    
         kworker/0:2-584   [000] ....    72.396657: mc_event: 1 Corrected error: reserved error (18) on unknown label (mc:0 location:-1:-1:-1 address:0x00000000 grain:1 syndrome:0x00000000 APEI location: node:3 card:0 module:0 status(0x0000000000000400): Storage error in DRAM memory)
    
    Tested on a 4 sockets E5-4650 Sandy Bridge machine.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

--- drivers/edac/edac_mc.c
+++ drivers/edac/edac_mc.c
@@ -1082,15 +1082,7 @@ void edac_raw_mc_handle_error(const enum hw_event_mc_err_type type,
 			      struct edac_raw_error_desc *e)
 {
 	char detail[80];
-	u8 grain_bits;
 	int pos[EDAC_MAX_LAYERS] = { e->top_layer, e->mid_layer, e->low_layer };
-
-	/* Report the error via the trace interface */
-	grain_bits = fls_long(e->grain) + 1;
-	trace_mc_event(type, e->msg, e->label, e->error_count,
-		       mci->mc_idx, e->top_layer, e->mid_layer, e->low_layer,
-		       PAGES_TO_MiB(e->page_frame_number) | e->offset_in_page,
-		       grain_bits, e->syndrome, e->other_detail);
 
 	/* Memory type dependent details about the error */
 	if (type == HW_EVENT_ERR_CORRECTED) {
@@ -1148,6 +1140,7 @@ void edac_mc_handle_error(const enum hw_event_mc_err_type type,
 	int row = -1, chan = -1;
 	int pos[EDAC_MAX_LAYERS] = { top_layer, mid_layer, low_layer };
 	int i, n_labels = 0;
+	u8 grain_bits;
 	struct edac_raw_error_desc *e = &mci->error_desc;
 
 	edac_dbg(3, "MC%d\n", mci->mc_idx);
@@ -1286,6 +1279,13 @@ void edac_mc_handle_error(const enum hw_event_mc_err_type type,
 	}
 	if (p > e->location)
 		*(p - 1) = '\0';
+
+	/* Report the error via the trace interface */
+	grain_bits = fls_long(e->grain) + 1;
+	trace_mc_event(type, e->msg, e->label, e->error_count,
+		       mci->mc_idx, e->top_layer, e->mid_layer, e->low_layer,
+		       PAGES_TO_MiB(e->page_frame_number) | e->offset_in_page,
+		       grain_bits, e->syndrome, e->other_detail);
 
 	edac_raw_mc_handle_error(type, mci, e);
 }
